c5cc2479cf605e37ff3bbc5c2c7a48bab8856121
Moustafa Makhlouf <moustafa_makhlouf@brown.edu>
2020-11-23 18:51:59 -0500

    changes to probably undo

--- code/load.py
+++ code/load.py
@@ -1,5 +1,6 @@
 import os
 import pickle 
+import tensorflow as tf
 
 def load_obj(filename):
     load = {}
@@ -12,10 +13,10 @@ def load_obj(filename):
 
 def get_data(testing_data_path, testing_labels_path,training_data_path, training_labels_path):
     testing_data = load_obj(testing_data_path)
-    testing_labels = load_obj(testing_labels_path)
+    testing_labels = tf.one_hot(load_obj(testing_labels_path) ,32,dtype=tf.float32)
     training_data = load_obj(training_data_path)
-    training_labels = load_obj(training_labels_path)
-    print(len(testing_data), len(testing_labels), len(training_data), len(training_labels))
+    training_labels = tf.one_hot(load_obj(training_labels_path) ,32,dtype=tf.float32)
+    print(len(testing_data),testing_data[0].shape, len(testing_labels), len(training_data), len(training_labels))
     return testing_data, testing_labels, training_data, training_labels
 
 
